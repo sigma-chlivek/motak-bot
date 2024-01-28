50cd4f378fdd809fa9c890c690862851fd1af672
motakjan <jan.motak@hotmail.cz>
2024-01-28 02:37:21 +0100

    fix: remove unused import

--- src/commands/utils.rs
+++ src/commands/utils.rs
@@ -1,5 +1,3 @@
-use serenity::{model::channel::Message, prelude::*};
-
 pub fn parse_numeric(input_str: &str) -> Option<i32> {
     match input_str.parse::<i32>() {
         Ok(number) => Some(number),
